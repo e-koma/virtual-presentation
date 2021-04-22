359517906bfa8b9a0edb695ac971ec2001cf73c5
yuto.komai <yuto.komai@aktsk.jp>
2021-04-23 01:10:33 +0900

    samplePDF表示スクリプト追加

---
+++ Assets/MyAssets/Scripts/PDFCustomRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Paroxe.PdfRenderer;
+
+
+public class PDFCustomRenderer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PDFDocument pdfDocument = new PDFDocument("Assets/MyAssets/PDFs/game_serverside.pdf", "");
+        PDFPage page = new PDFPage(pdfDocument, 0);
+        Texture2D tex = pdfDocument.Renderer.RenderPageToTexture(page, 960, 540);
+        GetComponent<MeshRenderer>().material.mainTexture = tex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
