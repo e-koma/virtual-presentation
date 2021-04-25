00780aeba8333f5da204132b423f814a7c6173d2
yuto.komai <yuto.komai@aktsk.jp>
2021-04-26 02:15:06 +0900

    Enterクリックにより、プレゼン終了アニメーションに移行する & アニメーションループ追加

---
+++ Assets/MyAssets/Scripts/AnimationManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyUp("enter") || Input.GetKeyUp("return"))
+        {
+            animator.SetTrigger("ClickEnter");
+        }
+    }
+}
