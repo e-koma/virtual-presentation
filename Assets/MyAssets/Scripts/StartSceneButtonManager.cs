ce0fa90143525fa1110a88454765d695dc6a500c
yuto.komai <yuto.komai@aktsk.jp>
2021-04-29 20:58:24 +0900

    ボタンクリック用スクリプト追加

---
+++ Assets/MyAssets/Scripts/StartSceneButtonManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SFB;
+
+public class StartSceneButtonManager : MonoBehaviour
+{
+    public void OnClickedStartButton()
+    {
+        string pdfFileName = LoadPDFFile();
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClickedExitButton()
+    {
+        QuitSystem();
+    }
+
+    private string LoadPDFFile()
+    {
+        ExtensionFilter[] extensions = new[] {
+            new ExtensionFilter("PDF File", "pdf"),
+        };
+        string[] paths = StandaloneFileBrowser.OpenFilePanel("Open PDF File", "", extensions, false);
+        if (paths[0] == "")
+        {
+            QuitSystem();
+        }
+
+        return paths[0];
+    }
+
+    private void QuitSystem()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+}
