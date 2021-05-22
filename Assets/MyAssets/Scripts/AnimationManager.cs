63eeccc023c1ed11e1887c69fa1fcd048a9aa54b
yuto.komai <yuto.komai@aktsk.jp>
2021-05-23 00:28:05 +0900

    アニメーションstate制御を入れる

--- Assets/MyAssets/Scripts/AnimationManager.cs
+++ Assets/MyAssets/Scripts/AnimationManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour
 {
     private Animator animator;
+    private enum PresenState { BeforeStage, EnterStage, StartPresen, FinishPresen }
+    private int presenState = (int)PresenState.BeforeStage;
 
     void Start()
     {
@@ -13,10 +15,39 @@ public class AnimationManager : MonoBehaviour
 
     void LateUpdate()
     {
-        if (Input.GetKeyUp("enter") || Input.GetKeyUp("return"))
+        switch (presenState)
         {
-            animator.applyRootMotion = true;
-            animator.SetTrigger("ClickEnter");
+            case (int)PresenState.BeforeStage:
+                if (isClickEnter())
+                {
+                    animator.applyRootMotion = true;
+                    animator.SetTrigger("EnterStage");
+                    presenState = (int)PresenState.EnterStage;
+                }
+                break;
+            case (int)PresenState.EnterStage:
+                if (isClickEnter())
+                {
+                    animator.applyRootMotion = false;
+                    animator.SetTrigger("StartPresen");
+                    presenState = (int)PresenState.StartPresen;
+                }
+                break;
+            case (int)PresenState.StartPresen:
+                if (isClickEnter())
+                {
+                    animator.applyRootMotion = true;
+                    animator.SetTrigger("FinishPresen");
+                    presenState = (int)PresenState.FinishPresen;
+                }
+                break;
+            default:
+                break;
         }
+    }
+
+    bool isClickEnter()
+    {
+        return Input.GetKeyUp("enter") || Input.GetKeyUp("return");
     }
 }
