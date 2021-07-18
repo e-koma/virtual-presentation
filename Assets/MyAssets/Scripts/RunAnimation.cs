312e7e5894accb5a1ded115cba3425b1974a445c
yuto.komai <yuto.komai@aktsk.jp>
2021-07-18 18:22:55 +0900

    キャラクター移動時に回転ができるようにする

--- Assets/MyAssets/Scripts/RunAnimation.cs
+++ Assets/MyAssets/Scripts/RunAnimation.cs
@@ -5,8 +5,10 @@ using UnityEngine.InputSystem;
 
 public class RunAnimation : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 2.0f;
 
+    private GameObject myGameObject;
+    private Vector3 latestPosition;
     private Animator animator;
     private CharacterController charaController;
     private Vector3 moveDirection;
@@ -15,9 +17,11 @@ public class RunAnimation : MonoBehaviour
 
     void Start()
     {
+        this.myGameObject = this.gameObject;
         this.animator = this.GetComponent<Animator>();
         this.charaController = this.GetComponent<CharacterController>();
         this.moveDirection = new Vector3(0, 0, 0);
+        this.latestPosition = new Vector3(0, 0, 0);
     }
 
     void LateUpdate()
@@ -31,5 +35,13 @@ public class RunAnimation : MonoBehaviour
         moveDirection.z = padYValue;
         moveDirection.y = Physics.gravity.y;
         charaController.Move(moveDirection * Time.deltaTime);
+
+        Vector3 diff = moveDirection - latestPosition;
+        latestPosition = moveDirection;
+
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(diff * Time.deltaTime);
+        }
     }
 }
